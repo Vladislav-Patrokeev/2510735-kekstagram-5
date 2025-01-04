61de4e1b203cf76ed99c3ca2630b5b3e5b48253c
Vladislav-Patrokeev <ihavealvxframe@gmail.com>
2025-01-04 15:04:01 +0500

    правка1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
-// Импорт функций из других модулей
-import { getRandomInt, getRandomElement } from './utils.js';
-import { descriptions, commentsTexts, names, generateComment, generatePhotos } from './data.js';
-import {renderPhotos} from './pictures.js';
-
-// Генерация массива фотографий
-const photos = generatePhotos();
-
-console.log(photos);
-
-// Отрисовка миниатюр
-renderPhotos();
+import './gallery.js';
+import './form.js';
